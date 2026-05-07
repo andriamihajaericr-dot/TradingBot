c8e366bf3472f49e6611e58b88d5fbd45e2a7843
andriamihajaericr-dot <andriamihajaericr@gmail.com>
2026-05-07 17:10:17 +0300

    Update MainActivity.java

--- app/src/main/java/com/tradingbot/analyzer/MainActivity.java
+++ app/src/main/java/com/tradingbot/analyzer/MainActivity.java
@@ -11,6 +11,8 @@ import java.util.Set;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
