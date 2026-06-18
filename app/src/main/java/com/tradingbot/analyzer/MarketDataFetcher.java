601e3e57c5b0596b1ace4188159ac75cbae0c6bd
andriamihajaericr-dot <andriamihajaericr@gmail.com>
2026-06-18 23:14:43 +0300

    Update MarketDataFetcher.java

--- app/src/main/java/com/tradingbot/analyzer/MarketDataFetcher.java
+++ app/src/main/java/com/tradingbot/analyzer/MarketDataFetcher.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MarketDataFetcher {
     private static final String TAG = "MarketDataFetcher";
