384b314fbe3ed708d0aece58d928aa2caa91f22a
andriamihajaericr-dot <andriamihajaericr@gmail.com>
2026-07-01 17:22:58 +0300

    Update TradingViewFetcher.java

--- app/src/main/java/com/tradingbot/analyzer/TradingViewFetcher.java
+++ app/src/main/java/com/tradingbot/analyzer/TradingViewFetcher.java
@@ -5,7 +5,8 @@ import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+import java.util.LinkedHashMap;
+import java.io.InputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
