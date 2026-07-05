c75d21ed3bc242abee7ed2535ec21f910573242f
andriamihajaericr-dot <andriamihajaericr@gmail.com>
2026-07-05 22:26:21 +0300

    Update TradingViewFetcher.java

--- app/src/main/java/com/tradingbot/analyzer/TradingViewFetcher.java
+++ app/src/main/java/com/tradingbot/analyzer/TradingViewFetcher.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
